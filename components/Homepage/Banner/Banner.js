a293255bddc9ab88bf14394f712b028f1195864a
Abhishek Adhikari <abhiayush23@gmail.com>
2021-05-21 12:21:52 +0530

    font size check

---
+++ components/Homepage/Banner/Banner.js
@@ -0,0 +1,16 @@
+import s from './Banner.module.scss';
+
+const Banner = () => {
+	return (
+		<div>
+			<div className={s.div}>
+				<h1>VISIT ASAKUSA</h1>
+				<h2>VISIT ASAKUSA</h2>
+				<h3>VISIT ASAKUSA</h3>
+				<h4>VISIT ASAKUSA</h4>
+			</div>
+		</div>
+	);
+};
+
+export default Banner;
